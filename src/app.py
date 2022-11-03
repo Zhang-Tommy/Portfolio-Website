eee48d6b39d07b164af066d050f85ff4fae2408f
Tommy Zhang <tommyz@uw.edu>
2022-11-03 11:15:26 -0700

    test

--- src/app.py
+++ src/app.py
@@ -7,13 +7,18 @@ from waitress import serve
 os.environ['FLASK_APP'] = 'main'
 app = Flask(__name__)
 
-app.config['SERVER_NAME'] = 'tommy-zhang.com'
-
 # Routing to home page
 # On form submission, redirect to start distribution fitting
 @app.route("/")
 def index():
     return render_template('./index.html', methods=['GET', 'POST'])
 
+@app.route("/get_resume")
+def get_resume():
+    return render_template('./resume_viewer.html', methods=['GET', 'POST'])
+
+@app.route('/pdf/') #the url you'll send the user to when he wants the pdf
+def pdfviewer():
+    return send_file("templates/TZ_Resume.pdf") #the pdf itself
 
 serve(app, listen='*:8080')
